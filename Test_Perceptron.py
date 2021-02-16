c1a5811be61051792d985dfa045862eccc2b4ea3
Thijme Wortel <60653407+Kinggoid@users.noreply.github.com>
2021-02-16 10:28:04 +0100

    laatste commit.

--- Test_Perceptron.py
+++ Test_Perceptron.py
@@ -1,8 +1,8 @@
 import unittest
 
-from ML.Perceptron_laag import PerceptronLaag
-from ML.perceptron_netwerk import PerceptronNetwork
-from ML.perceptronen import Perceptron
+from ML.Perceptron.Perceptron_laag import PerceptronLaag
+from ML.Perceptron.perceptron_netwerk import PerceptronNetwork
+from ML.Perceptron.perceptronen import Perceptron
 
 
 class test_perceptronen(unittest.TestCase):
@@ -12,29 +12,26 @@ class test_perceptronen(unittest.TestCase):
         for i in range(0, 2):
             for j in range(0, 2):
                 AND = Perceptron([1, 1], 2, 'AND gate')
-                antwoorden.append(AND.caclulate_output([i, j]))
+                antwoorden.append(AND.calculate_output([i, j]))
 
         self.assertEqual(antwoorden, [0,0,0,1])  # Kijk of de outputs goed zijn
-
 
     def test_NOT_GATE(self):
         antwoorden = []  # Kijk per input wat het antwoord zou zijn.
         for i in range(0, 2):
             NOT = Perceptron([-1], 0, 'NOT gate')
-            antwoorden.append(NOT.caclulate_output([i]))
+            antwoorden.append(NOT.calculate_output([i]))
 
         self.assertEqual(antwoorden, [1, 0])  # Kijk of de outputs goed zijn
-
 
     def test_OR_GATE(self):
         antwoorden = []  # Kijk per input wat het antwoord zou zijn.
         for i in range(0, 2):
             for j in range(0, 2):
                 OR = Perceptron([1, 1], 1, 'OR gate')
-                antwoorden.append(OR.caclulate_output([i, j]))
+                antwoorden.append(OR.calculate_output([i, j]))
 
         self.assertEqual(antwoorden, [0, 1, 1, 1])  # Kijk of de outputs goed zijn
-
 
     def test_NOR_GATE(self):
         antwoorden = []  # Kijk per input wat het antwoord zou zijn.
@@ -42,10 +39,9 @@ class test_perceptronen(unittest.TestCase):
             for j in range(0, 2):
                 for u in range(0, 2):
                     NOR = Perceptron([-1, -1, -1], 0, 'NOR gate')
-                    antwoorden.append(NOR.caclulate_output([i, j, u]))
+                    antwoorden.append(NOR.calculate_output([i, j, u]))
 
         self.assertEqual(antwoorden, [1, 0, 0, 0, 0, 0, 0, 0])  # Kijk of de outputs goed zijn
-
 
     def test_3_WAY_GATE(self):
         antwoorden = []  # Kijk per input wat het antwoord zou zijn.
@@ -53,14 +49,32 @@ class test_perceptronen(unittest.TestCase):
             for j in range(0, 2):
                 for u in range(0, 2):
                     NOR = Perceptron([0.6, 0.3, 0.2], 0.4, 'NOR gate')
-                    antwoorden.append(NOR.caclulate_output([i, j, u]))
+                    antwoorden.append(NOR.calculate_output([i, j, u]))
 
         self.assertEqual(antwoorden, [0, 0, 0, 1, 1, 1, 1, 1])  # Kijk of de outputs goed zijn
 
-
-    def test_str(self):
+    def test_str_perceptron(self):
         percep = Perceptron([1, 1], 2, 'AND gate')
-        self.assertEqual(percep.__str__(), 'Mijn naam is AND gate en ik heb 2 input variabelen. Mijn treshold is 2 en mijn bias is 0.')  # Kijk of de outputs goed zijn
+        self.assertEqual(percep.__str__(), 'Mijn naam is AND gate en ik heb 2 input variabelen. Mijn threshold is 0 en mijn bias is 2.')  # Kijk of de outputs goed zijn
+
+    def test_str_perceptronlaag(self):
+        x1_1 = Perceptron([1, 1], 2, 'x1_1')
+        x1_2 = Perceptron([1, 1], 2, 'x1_2')
+
+        laag_een = PerceptronLaag([x1_1, x1_2], 'L1')
+        self.assertEqual(laag_een.__str__(), 'Ik ben de perceptronlaag L1 en ik bevat de volgende perceptronen: x1_1, x1_2')  # Kijk of de outputs goed zijn
+
+    def test_str_perceptronnetwork(self):
+        x1_1 = Perceptron([1, 1], 2, 'x1_1')
+        x1_2 = Perceptron([1, 1], 2, 'x1_2')
+        x2_1 = Perceptron([1, 1], 2, 'x2_1')
+        x2_2 = Perceptron([1, 1], 2, 'x2_2')
+
+        laag_een = PerceptronLaag([x1_1, x1_2], 'L1')
+        laag_twee = PerceptronLaag([x2_1, x2_2], 'L2')
+
+        network = PerceptronNetwork([laag_een, laag_twee])
+        self.assertEqual(network.__str__(), 'In dit network zitten de volgende lagen: L1, L2')  # Kijk of de outputs goed zijn
 
 
 class Test_Network(unittest.TestCase):
@@ -69,21 +83,20 @@ class Test_Network(unittest.TestCase):
         x1_1 = Perceptron([1, 1], 1, 'x1_1')
         x1_2 = Perceptron([-1, -1], -1.5, 'x1_2')
 
-        laag_een = PerceptronLaag([x1_1, x1_2])
+        laag_een = PerceptronLaag([x1_1, x1_2], 'L1')
 
         x2_1 = Perceptron([1, 1], 2, 'x2_1')
 
-        laag_twee = PerceptronLaag([x2_1])
+        laag_twee = PerceptronLaag([x2_1], 'L2')
 
         XOR = PerceptronNetwork([laag_een, laag_twee])
 
         antwoorden = []  # Kijk per input wat het antwoord zou zijn.
         for i in range(0, 2):
             for j in range(0, 2):
-                antwoorden.append(XOR.network([i, j]))
+                antwoorden.append(XOR.feed_forward([i, j]))
 
         self.assertEqual(antwoorden, [[0], [1], [1], [0]])  # Kijk of de outputs goed zijn
-
 
     def test_half_adder(self):
         # Maak het network
@@ -91,19 +104,19 @@ class Test_Network(unittest.TestCase):
         x1_2 = Perceptron([-1, -1], -1.5, 'x1_1')
         x1_3 = Perceptron([1, 1], 2, 'x1_3')
 
-        laag_een = PerceptronLaag([x1_1, x1_2, x1_3])
+        laag_een = PerceptronLaag([x1_1, x1_2, x1_3], 'L1')
 
         x2_1 = Perceptron([1, 1, 0], 2, 'x2_1')
         x2_2 = Perceptron([0, 0, 1], 1, 'x2_2')
 
-        laag_twee = PerceptronLaag([x2_1, x2_2])
+        laag_twee = PerceptronLaag([x2_1, x2_2], 'L2')
 
         Half_adder = PerceptronNetwork([laag_een, laag_twee])
 
         antwoorden = []  # Kijk per input wat het antwoord zou zijn.
         for i in range(0, 2):
             for j in range(0, 2):
-                antwoorden.append(Half_adder.network([i, j]))
+                antwoorden.append(Half_adder.feed_forward([i, j]))
 
         self.assertEqual(antwoorden, [[0, 0], [1, 0], [1, 0], [0, 1]])  # Kijk of de outputs goed zijn
 
--- perceptronen.py
+++ perceptronen.py
@@ -1,21 +1,23 @@
-class Perceptron:
-    def __init__(self, weights: list, treshold: float, name: str):
-        self.weights = weights
-        self.treshold = treshold
-        self.name = name
-        self.bias = 0
+from typing import List
 
-    def caclulate_output(self, inputs):
+class Perceptron:
+    def __init__(self, weights: List[float], bias: float, name: str):
+        self.weights = weights
+        self.threshold = 0
+        self.name = name
+        self.bias = bias
+
+    def calculate_output(self, inputs: List[int]):
         """In deze definitie kijken we bij een perceptron welke inputs hij krijgt en hoe de weights en de bias dit
-        beïnvloeden. Vervolgens kijken we of het antwoord de treshold haalt."""
+        beïnvloeden. Vervolgens kijken we of het antwoord de threshold haalt."""
         inputs_met_weight = [self.weights[i] * inputs[i] for i in range(0, len(inputs))]  # Inputs * weights
-        if sum(inputs_met_weight) + self.bias >= self.treshold:  # De som van die resultaten tellen we op met de bias en dan kijken we of het de treshold haalt.
-            return True
-        return False
+        if sum(inputs_met_weight) - self.bias >= self.threshold:  # De som van die resultaten tellen we op met de bias en dan kijken we of het de threshold haalt.
+            return 1
+        return 0
 
     def __str__(self):
         """Informatie van de perceptron"""
-        return 'Mijn naam is {} en ik heb {} input variabelen. Mijn treshold is {} en mijn bias is {}.'.format(self.name, str(len(self.weights)), str(self.treshold), str(self.bias))
+        return 'Mijn naam is {} en ik heb {} input variabelen. Mijn threshold is {} en mijn bias is {}.'.format(self.name, str(len(self.weights)), str(self.threshold), str(self.bias))
 
 
 
